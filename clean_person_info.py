a260af65ee73fbce5e4f328e262360d8e64ab1d5
Artem Schennikov <35872058+Schtschennikow@users.noreply.github.com>
2020-08-07 17:24:52 +0300

    Create clean_person_info.py

---
+++ clean_person_info.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import sqlalchemy
+import json
+
+
+argparser = argparse.ArgumentParser(description='Поиск персон из выписок ЕГРЮЛ в базе')
+requiredNamed = argparser.add_argument_group('required arguments')
+requiredNamed.add_argument('--persons_egul', type=str, help="таблица с данными о персонах из ЕГРЮЛ", required=True)
+
+args = argparser.parse_args()
+
+params = json.loads(open("params.json").read())
+
+engine = sqlalchemy.create_engine(params["engine"])
+
+query_persons = params["query_persons"]
+df = pd.read_sql_query(query_persons, engine)
+
+df["name_key"] = df.apply(lambda x: get_n_key(
+    x["family_name"], 
+    x["name"], 
+    x["patronymic"]
+), axis=1)
+
+            
+df = df.astype(str).drop_duplicates()
+df = df.astype(str).drop_duplicates(["legal_entity_id", "name_key"])
+
+pers_inn = pd.read_csv(args.persons_egul)
+pers_inn = pd.merge(df_inn, df, how="inner", on=["legal_entity_id", "name_key"])#.drop_duplicates()
+
+pers_inn.date = pd.to_datetime(pers_inn.date, format="%d.%m.%Y")
+
+for i, g in pers_inn.groupby("legal_entity_id"):
+    G = g.sort_values("date")
+    if G.shape[0]>1:
+        G.reset_index(inplace=True)
+        for I, R in G[:-1].iterrows():
+            pers_inn.loc[R["index"], "end_date"] = G.iloc[I+1].date
+            
+pers_inn.drop(columns=["name_key"], inplace=True)
+
+pers_inn = pers_inn.astype(str)
+pers_inn.to_csv("persons_egul_done.csv", index=False)
